53f33eb600b2f3ea8df3c46e6a6f762afee33e21
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-23 09:37:31 +0300

    Exercise 7: Multiplication

--- print/src/Hello.java
+++ print/src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         int firstVariable = 10;
         int secondVariable = 33;
-        int sum = firstVariable + secondVariable;
-        System.out.println(firstVariable + " + " + secondVariable + " = " + sum);
+        int sum = firstVariable * secondVariable;
+        System.out.println(firstVariable + " * " + secondVariable + " = " + sum);
     }
 }
