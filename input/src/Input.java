6ab11ae4e8825d0186db38ce475763d8167faf1d
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 11:12:59 +0300

    Exercise 27: From one to a hundred

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,19 +2,10 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        while (true) {
-            int read = Integer.parseInt(reader.nextLine());
-            if (read == 0) {
-                break;
-            }
-
-            sum = sum + read;
-
-            System.out.println("Sum now: " + sum);
+        int number = 1;
+        while (number <= 100) {
+            System.out.println(number);
+            number++;
         }
-
-        System.out.println("Sum in the end: " + sum);
     }
 }
