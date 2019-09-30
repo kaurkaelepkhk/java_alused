78547c0cea50cba06bdabdbefb9ccd1d288ba70f
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 10:40:25 +0300

    Exercise 18: Grades and points

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,16 +2,23 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        System.out.println("Type a first number");
+        System.out.println("Type the points [0-60]");
         Scanner reader = new Scanner(System.in);
-        int firstValue = Integer.parseInt(reader.nextLine());
-        System.out.println("Type a second number");
-        int secondValue = Integer.parseInt(reader.nextLine());
-        if (firstValue == secondValue) {
-            System.out.println("The numbers are equal");
+        int value = Integer.parseInt(reader.nextLine());
+        if (value >= 0 && value <= 29) {
+            System.out.println("Grade: failed");
+        } else if (value >= 30 && value <= 34) {
+            System.out.println("Grade: 1");
+        } else if (value >= 35 && value <= 39) {
+            System.out.println("Grade: 2");
+        } else if (value >= 40 && value <= 44) {
+            System.out.println("Grade: 3");
+        } else if (value >= 45 && value <= 49) {
+            System.out.println("Grade: 4");
+        } else if (value >= 50 && value <= 60) {
+            System.out.println("Grade: 5");
         } else {
-            int sum = Math.max(firstValue, secondValue);
-            System.out.println("The bigger number is: " + sum);
+            System.out.println("Enter a valid number within the scope");
         }
     }
 }
