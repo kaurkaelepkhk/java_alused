889ca034e25a27dc3254a96f609c579f0cce9341
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 14:30:39 +0300

    Exercise 36.4: Counting the average Exercise 36.5: Even and odd numbers

--- input/src/Input.java
+++ input/src/Input.java
@@ -6,14 +6,24 @@ public class Input {
         int value = 0;
         int sum = 0;
         int amount = 0;
+        int even = 0;
+        int odd = 0;
         while (value != -1) {
             System.out.println("Type a number");
             value = Integer.parseInt(reader.nextLine());
             sum += value;
             amount++;
+            if (value % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
         }
         System.out.println("Thank you");
         System.out.println("The sum is " + (sum + 1));
         System.out.println("You typed a number " + (amount - 1) + " times");
+        System.out.println("Average: " + (sum + 1) / ( amount -1 ));
+        System.out.println("Amount of even numbers: " + even);
+        System.out.println("Amount of odd numbers: " + odd);
     }
 }
