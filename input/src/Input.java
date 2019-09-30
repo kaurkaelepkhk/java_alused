025859132b12d22479b8df1ff0c5716497987519
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 10:18:58 +0300

    Exercise 14: A positive number

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,16 +2,10 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        System.out.println("Type your name");
+        System.out.println("Type a number");
         Scanner reader = new Scanner(System.in);
-        String firstName = reader.nextLine();
-        System.out.println("Type your age");
-        int firstAge = Integer.parseInt(reader.nextLine());
-        System.out.println("Type your name");
-        String secondName = reader.nextLine();
-        System.out.println("Type your age");
-        int secondAge = Integer.parseInt(reader.nextLine());
-        int sum = firstAge + secondAge;
-        System.out.println(firstName + " and " + secondName + " are " + sum + " years old in total");
+        int arv = Integer.parseInt(reader.nextLine());
+        String arvCalc = (arv >= 0) ? "positive" : "negative";
+        System.out.println("The number is " + arvCalc);
     }
 }
