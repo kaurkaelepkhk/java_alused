56835fba6a2b62ac2606aeec0e2e493e69e10293
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 14:26:26 +0300

    Exercise 36.3: Summing and counting the numbers

--- input/src/Input.java
+++ input/src/Input.java
@@ -5,12 +5,15 @@ public class Input {
         Scanner reader = new Scanner(System.in);
         int value = 0;
         int sum = 0;
+        int amount = 0;
         while (value != -1) {
             System.out.println("Type a number");
             value = Integer.parseInt(reader.nextLine());
             sum += value;
+            amount++;
         }
         System.out.println("Thank you");
         System.out.println("The sum is " + (sum + 1));
+        System.out.println("You typed a number " + (amount - 1) + " times");
     }
 }
