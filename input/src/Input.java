98f69b8c02a88b3ccbf5868e30f40ac34cd2688b
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 11:22:25 +0300

    Exercise 31: Lower limit and upper limit

--- input/src/Input.java
+++ input/src/Input.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Up to what number?");
-        int value = Integer.parseInt(reader.nextLine());
-        int number = 1;
-        while (number <= value) {
-            System.out.println(number);
-            number++;
+        System.out.println("First");
+        int value1 = Integer.parseInt(reader.nextLine());
+        System.out.println("Second");
+        int value2 = Integer.parseInt(reader.nextLine());
+        while (value1 <= value2) {
+            if (value1 > value2) {
+                break;
+            }
+            System.out.println(value1);
+            value1++;
         }
     }
 }
