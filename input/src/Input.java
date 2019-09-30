e897f52cfde658e0697e3de001d88354678bdc9a
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 14:18:58 +0300

    Exercise 36.1: Reading numbers

--- input/src/Input.java
+++ input/src/Input.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type a number");
-        int value = Integer.parseInt(reader.nextLine());
-        int result = (int)Math.pow(2, value);
-        System.out.println("The result is " + (result * 2 - 1));
+        int value = 0;
+        while (value != -1) {
+            System.out.println("Type a number");
+            value = Integer.parseInt(reader.nextLine());
+        }
+        System.out.println("Thank you");
     }
 }
