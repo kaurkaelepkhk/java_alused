86135a431e759f00c1d49f440fb5b66ab17abc81
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 11:18:40 +0300

    Exercise 30: Up to a certain number

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Up to what number?");
+        int value = Integer.parseInt(reader.nextLine());
         int number = 1;
-        while (number <= 100) {
-            if(number % 2 == 0) {
-                System.out.println(number);
-            }
+        while (number <= value) {
+            System.out.println(number);
             number++;
         }
     }
