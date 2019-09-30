8d6d9b126b3e5fbd8292e722e857f6a9ebaab1b4
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 15:07:08 +0300

    Exercise 39: Printing

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,16 +2,44 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("How many times");
-        int value = Integer.parseInt(reader.nextLine());
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("How many times");
+//        int value = Integer.parseInt(reader.nextLine());
+//        printSquare(4);
+//        printSquare(4);
+//        printSquare(4);
+//        printRectangle(17, 3);
+        printTriangle(4);
+    }
+    public static void printStars(int amountOfStars) {
+            System.out.print("*");
+    }
+    public static void printSquare(int amount) {
         int i = 0;
-        while (i < value) {
+        while (i < amount) {
             i++;
-            printText();
+            printStars(amount);
+        }
+        System.out.print("\r\n");
+    }
+    public static void printRectangle(int width, int height) {
+        int i, j;
+        for(i = 1; i <= height; i++)
+        {
+            for(j = 1; j <= width; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
         }
     }
-    public static void printText() {
-        System.out.println("In the beginning there were the swamp, the hoe and Java." + "\n");
+    public static void printTriangle(int size) {
+        int i, j;
+        for (i = 0; i <= size; i = i + 1) {
+            for (j = 0; j < i; j++) {
+                printStars(size);
+            }
+            System.out.println();
+        }
     }
 }
