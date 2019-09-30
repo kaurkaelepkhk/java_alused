646d31019522ad341bfe8701b08e5aa50b30e080
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 10:21:18 +0300

    Exercise 15: Age of majority

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        System.out.println("Type a number");
+        System.out.println("Type your age");
         Scanner reader = new Scanner(System.in);
         int arv = Integer.parseInt(reader.nextLine());
-        String arvCalc = (arv >= 0) ? "positive" : "negative";
-        System.out.println("The number is " + arvCalc);
+        String arvCalc = (arv >= 18) ? "have" : "have not";
+        System.out.println("You " + arvCalc + " reached the age of majority");
     }
 }
