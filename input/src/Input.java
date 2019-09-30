d5c7c78d02b0cd0c5e889adce13743f0464b124b
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 14:24:26 +0300

    Exercise 36.2: The sum of the numbers

--- input/src/Input.java
+++ input/src/Input.java
@@ -4,10 +4,13 @@ public class Input {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int value = 0;
+        int sum = 0;
         while (value != -1) {
             System.out.println("Type a number");
             value = Integer.parseInt(reader.nextLine());
+            sum += value;
         }
         System.out.println("Thank you");
+        System.out.println("The sum is " + (sum + 1));
     }
 }
