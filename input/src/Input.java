37527c8a60eb47c0156636aa2e65a22f5792fcca
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 14:36:28 +0300

    Exercise 38: Many prints

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        printText();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("How many times");
+        int value = Integer.parseInt(reader.nextLine());
+        int i = 0;
+        while (i < value) {
+            i++;
+            printText();
+        }
     }
     public static void printText() {
         System.out.println("In the beginning there were the swamp, the hoe and Java." + "\n");
