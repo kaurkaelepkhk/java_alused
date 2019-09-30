d57c72ccc64820190c4a456def2ec4f84e8fa686
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 10:23:15 +0300

    Exercise 16: Even or odd?

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        System.out.println("Type your age");
+        System.out.println("Type a number");
         Scanner reader = new Scanner(System.in);
         int arv = Integer.parseInt(reader.nextLine());
-        String arvCalc = (arv >= 18) ? "have" : "have not";
-        System.out.println("You " + arvCalc + " reached the age of majority");
+        String arvCalc = (arv % 2 == 0) ? "odd" : "even";
+        System.out.println("Number " + arv + " is " + arvCalc);
     }
 }
