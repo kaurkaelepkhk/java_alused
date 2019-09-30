b4d3e1f79941cff7f391ab05b994f09271b0a03a
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 11:14:51 +0300

    Exercise 28: From hundred to one

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        int number = 1;
-        while (number <= 100) {
+        int number = 100;
+        while (number >= 1) {
             System.out.println(number);
-            number++;
+            number--;
         }
     }
 }
