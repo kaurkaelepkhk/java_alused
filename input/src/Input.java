1d92eacc716dd4de902f3a5634bdcba04955a57d
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-09-30 11:01:27 +0300

    Exercise 22: Password

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        System.out.println("Enter a year");
-        Scanner reader = new Scanner(System.in);
-        int value = Integer.parseInt(reader.nextLine());
-        if (value % 4 == 0) {
-            System.out.println("The year is a leap year");
-        } else if ((value % 100) == 0 && (value % 400) == 0) {
-            System.out.println("The year is a leap year");
-        } else {
-            System.out.println("The year is not a leap year");
+        while (true) {
+            System.out.println("Enter your password");
+            Scanner reader = new Scanner(System.in);
+            String password = reader.nextLine();
+            if( password.equals("carrot")) {
+                System.out.println("Right");
+                System.out.println("Secret is: Secret");
+                break;
+            } else if (!password.equals("carrot")) {
+                System.out.println("Wrong");
+            }
         }
     }
 }
