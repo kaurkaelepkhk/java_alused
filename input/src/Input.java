0b4709158799512eef13d505834c64ad714dc352
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 12:01:47 +0300

    Exercise 57: Words

--- input/src/Input.java
+++ input/src/Input.java
@@ -1,18 +1,19 @@
+import java.util.*;
 import java.util.Scanner;
 
 public class Input {
-    public static String reverse(String text) {
-        int i;
-        String reversed = "";
-        for (i = text.length() - 1; i >= 0; i--) {
-            reversed+=text.charAt(i);
-        }
-        return reversed;
-    }
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Type in your text: ");
-        String output = reader.nextLine();
-        System.out.println("In reverse order: " + reverse(output));
+        ArrayList<String> words = new ArrayList<String>();
+        while (!words.contains("")) {
+            System.out.print("Type a word: ");
+            String output = reader.nextLine();
+            words.add(output);
+        }
+        System.out.println("You typed the following words: ");
+        for (String word : words) {
+            System.out.println(word);
+        }
     }
 }
