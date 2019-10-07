f931fb7ee9d10a738dbef42b8db6ac6158343f53
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 10:26:42 +0300

    Exercise 45: Greatest

--- input/src/Input.java
+++ input/src/Input.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Input {
-    public static int least(int a, int b) {
-        return Math.min(a, b);
+    public static int greatest(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
     }
     public static void main(String[] args) {
-        int answer = least(2, 7);
-        System.out.println("Least: " + answer);
+        int answer =  greatest(2, 7, 3);
+        System.out.println("Greatest: " + answer);
     }
 }
