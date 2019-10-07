44510081b7104bdc793ab00df7d80bbc2161fc21
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 12:15:38 +0300

    Exercise 61: Amount of items in a list

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,19 +2,35 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Input {
-
+    public static void print(ArrayList<String> printed) {
+        for (String word : printed) {
+            System.out.println( word );
+        }
+    }
+    public static int countItems(ArrayList<String> list) {
+        int i = 0;
+        for (String word : list) {
+            i++;
+        }
+        return i;
+    }
+    public static void removeFirst(ArrayList<String> list) {
+        list.remove(0);
+    }
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        ArrayList<String> words = new ArrayList<String>();
-        while (!words.contains("")) {
-            System.out.print("Type a word: ");
-            String output = reader.nextLine();
-            words.add(output);
-        }
-        System.out.println("You typed the following words: ");
-        Collections.sort(words);
-        for (String word : words) {
-            System.out.println(word);
-        }
+        ArrayList<String> programmingLanguages = new ArrayList<String>();
+        programmingLanguages.add("Pascal");
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("Ruby");
+        programmingLanguages.add("C++");
+
+        print(programmingLanguages);
+
+        removeFirst(programmingLanguages);
+
+        System.out.println();  // prints an empty line
+
+        print(programmingLanguages);
     }
 }
