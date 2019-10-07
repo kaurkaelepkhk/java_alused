166d3cc4630795aa4b008bd62ae7ea45f6da5dd8
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 10:44:34 +0300

    Exercise 50: Separating first characters

--- input/src/Input.java
+++ input/src/Input.java
@@ -5,6 +5,14 @@ public class Input {
         Scanner reader = new Scanner(System.in);
         System.out.println("Type your name");
         String value = reader.nextLine();
-        System.out.println("Last character: " + value.charAt(value.length() - 1));
+        int i;
+        for (i = 0; i < value.length(); i++) {
+            if(i >= 3) {
+                return;
+            } else if ( value.length() < 3) {
+                return;
+            }
+            System.out.println((i + 1) + ". character: " + value.charAt(i));
+        }
     }
 }
