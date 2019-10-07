9b27bedff8b3d48bc7a12a2f622acf3f361cc594
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 12:05:38 +0300

    Exercise 58: Recurring word

--- input/src/Input.java
+++ input/src/Input.java
@@ -6,14 +6,15 @@ public class Input {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         ArrayList<String> words = new ArrayList<String>();
-        while (!words.contains("")) {
+        String theWord = "";
+        while (theWord.isEmpty()) {
             System.out.print("Type a word: ");
             String output = reader.nextLine();
+            if(words.contains(output)) {
+                theWord += output;
+            }
             words.add(output);
         }
-        System.out.println("You typed the following words: ");
-        for (String word : words) {
-            System.out.println(word);
-        }
+        System.out.println("You typed the word " + theWord + " twice");
     }
 }
