01b529c6d79d573ab42ae44447fbce8a001f7d4d
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 11:31:14 +0300

    Exercise 55: A word inside a word

--- input/src/Input.java
+++ input/src/Input.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type a word");
-        String word = reader.nextLine();
-        System.out.println("Length of the end part");
-        int lengthOfWord = Integer.parseInt(reader.nextLine());
-        System.out.println("Result: " + word.substring(word.length() - lengthOfWord));
+        System.out.println("Type the first word");
+        String word1 = reader.nextLine();
+        System.out.println("Type the second word");
+        String word2 = reader.nextLine();
+        if (word1.indexOf(word2) != -1) {
+            System.out.println("The word '" + word2 + "' is found in the word '" + word1 + "'");
+        } else {
+            System.out.println("The word '" + word2 + "' is not found in the word '" + word1 + "'");
+        }
     }
 }
