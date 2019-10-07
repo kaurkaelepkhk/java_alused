5efed9d53c71d7ad64aa816b89d15deecff6a11c
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 13:38:03 +0300

    Exercise 68: Is the number more than once in the list?

--- input/src/Input.java
+++ input/src/Input.java
@@ -1,42 +1,33 @@
 import java.util.*;
 
 public class Input {
-    public static int sum(ArrayList<Integer> list) {
-        int sum = 0;
-
-        for(int i : list){
-            sum = sum + i;
+    public static boolean moreThanOnce(ArrayList<Integer> list, int number) {
+        boolean helper = true;
+        for (int i : list) {
+            if (i == number ) {
+                helper = false;
+            }
+            number = i;
         }
-        return sum;
-    }
-
-    public static double average(ArrayList<Integer> list) {
-        double total = sum(list);
-        return total/list.size();
-    }
-
-    public static double variance(ArrayList<Integer> list) {
-        double average = average(list);
-        double variance_buffer = 0.0;
-        double variance_result;
-
-        for(int i : list){
-            variance_buffer = variance_buffer + Math.pow((i-average), 2);
-        }
-
-        variance_result = variance_buffer / (list.size()-1);
-
-        return variance_result;
+        return helper;
     }
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(3);
         list.add(2);
         list.add(7);
         list.add(2);
 
-        System.out.println("The variance is: " + variance(list));
+        System.out.println("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        if (moreThanOnce(list, number)) {
+            System.out.println(number + " appears more than once.");
+        } else {
+            System.out.println(number + " does not appear more than once.");
+        }
     }
 
 }
