630737dd9efe8930f8bd277a8198ec36d15ea970
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 11:02:07 +0300

    Exercise 53: First part

--- input/src/Input.java
+++ input/src/Input.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type your name");
-        String value = reader.nextLine();
-        String reverse = "";
-        int i;
-        for (i = value.length() - 1; i >= 0; i--) {
-            reverse = reverse + value.charAt(i);
-        }
-        System.out.println("In reverse order: " + reverse);
+        System.out.println("Type a word");
+        String word = reader.nextLine();
+        System.out.println("Length of the first part");
+        int wordlength = Integer.parseInt(reader.nextLine());
+        System.out.println("Result: " + word.substring(0, wordlength));
     }
 }
