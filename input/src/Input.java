1e7b10fd188226f6fd2b8dbfdc1997d67f6aec3f
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-07 10:22:59 +0300

    Exercise 43: Sum of numbers

--- input/src/Input.java
+++ input/src/Input.java
@@ -1,26 +1,12 @@
 import java.util.Scanner;
 
 public class Input {
+    public static int sum(int a, int b, int c, int d) {
+        int sum = a + b + c + d;
+        return sum;
+    }
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        int randomNumber = (int) (Math.random() * 50);
-        System.out.println(randomNumber);
-        int value = 0;
-        int guesses = 0;
-        while (value != randomNumber) {
-            System.out.println("Guess a number");
-            value = Integer.parseInt(reader.nextLine());
-            if (value < randomNumber) {
-                guesses++;
-                System.out.println("The number is greater");
-            } else if (value > randomNumber) {
-                guesses++;
-                System.out.println("The number is lesser");
-            } else {
-                System.out.println("You guessed correctly. The number was " + randomNumber);
-                System.out.println("It took you " + (guesses + 1) + " guesses to get the correct number");
-                break;
-            }
-        }
+        int answer = sum(4, 3, 6, 1);
+        System.out.println("sum: " + answer);
     }
 }
