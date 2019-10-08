f55ef2e88fd3d8783da6354506bebb1097d09361
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-08 14:36:40 +0300

    Exercise 72.2: Your first money transfer

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
-        Account account = new Account("Kaur",100);
-        account.deposit(20);
-        System.out.println(account.toString());
+        Account MattAccount = new Account("Matt's account",1000);
+        Account MyAccount = new Account("My account",0);
+        MattAccount.withdrawal(100);
+        MyAccount.deposit(20);
+        System.out.println(MattAccount.toString());
+        System.out.println(MyAccount.toString());
     }
 }
