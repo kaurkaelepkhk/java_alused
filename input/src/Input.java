0c7e309c8dfb040a8e58716673989e7cb70b95ea
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-08 14:23:07 +0300

    Exercise 71: Smart combining

--- input/src/Input.java
+++ input/src/Input.java
@@ -1,27 +1,26 @@
 import java.util.*;
 
 public class Input {
-    public static void combine(ArrayList<Integer> first, ArrayList<Integer> second) {
+    public static void smartCombine(ArrayList<Integer> first, ArrayList<Integer> second) {
         for (int a : second) {
-            first.add(a);
+            if (!first.contains(a)) {
+                first.add(a);
+            }
         }
     }
 
     public static void main(String[] args) {
+
         ArrayList<Integer> list1 = new ArrayList<Integer>();
         ArrayList<Integer> list2 = new ArrayList<Integer>();
 
-        list1.add(4);
-        list1.add(3);
+        Collections.addAll(list1, 4, 3);
 
-        list2.add(5);
-        list2.add(10);
-        list2.add(7);
+        Collections.addAll(list2, 5, 10, 4, 3, 7);
 
-        combine(list1, list2);
-
-        System.out.println(list1); // prints [4, 3, 5, 10, 7]
-
-        System.out.println(list2); // prints [5, 10, 7]
+        // remove comment when method ready
+        //smartCombine(list1, list2);
+        System.out.println(list1);
+        System.out.println(list2);
     }
 }
