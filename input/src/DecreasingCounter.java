1d0bc2c4aa88de311c3dddd58b692a8457f1f29c
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-22 08:59:26 +0300

    Exercise 75.1: Implementing method decrease()

---
+++ input/src/DecreasingCounter.java
@@ -0,0 +1,17 @@
+public class DecreasingCounter {
+    private int value;
+
+    public DecreasingCounter(int valueAtStart) {
+        this.value = valueAtStart;
+    }
+
+    public void printValue() {
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrease() {
+        this.value--;
+    }
+
+    // and here the rest of the methods
+}
--- input/src/Input.java
+++ input/src/Input.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
-        Multiplier threeMultiplier = new Multiplier(3);
-        System.out.println("threeMultiplier.multiply(2): " + threeMultiplier.multiply(2));
+        DecreasingCounter counter = new DecreasingCounter(10);
 
-        Multiplier fourMultiplier = new Multiplier(4);
-        System.out.println("fourMultiplier.multiply(2): " + fourMultiplier.multiply(2));
+        counter.printValue();
 
-        System.out.println("threeMultiplier.multiply(1): " + threeMultiplier.multiply(1));
-        System.out.println("fourMultiplier.multiply(1): " + fourMultiplier.multiply(1));
+        counter.decrease();
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
     }
 }
