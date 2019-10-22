89326772e6db4cbe459602ba07f70b19da1f3be3
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-22 08:54:24 +0300

    Exercise 74: Multiplier

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,7 +2,13 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
-        Product banana = new Product("Banana", 1.1, 13);
-        banana.printProduct();
+        Multiplier threeMultiplier = new Multiplier(3);
+        System.out.println("threeMultiplier.multiply(2): " + threeMultiplier.multiply(2));
+
+        Multiplier fourMultiplier = new Multiplier(4);
+        System.out.println("fourMultiplier.multiply(2): " + fourMultiplier.multiply(2));
+
+        System.out.println("threeMultiplier.multiply(1): " + threeMultiplier.multiply(1));
+        System.out.println("fourMultiplier.multiply(1): " + fourMultiplier.multiply(1));
     }
 }
