8f892d484ecb525d19ba53ceed023a90ea57831f
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-22 09:45:21 +0300

    Exercise 77.6: Multiple cards

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,16 +2,20 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
-        LyyraCard card = new LyyraCard(10);
-        System.out.println(card);
-
-        card.loadMoney(15);
-        System.out.println(card);
-
-        card.loadMoney(10);
-        System.out.println(card);
-
-        card.loadMoney(200);
-        System.out.println(card);
+        LyyraCard cardPekka = new LyyraCard(20);
+        LyyraCard cardBrian = new LyyraCard(30);
+        cardPekka.payGourmet();
+        cardBrian.payEconomical();
+        System.out.println("Pekka" + cardPekka);
+        System.out.println("Brian" + cardBrian);
+        cardPekka.loadMoney(20);
+        cardBrian.payGourmet();
+        System.out.println("Pekka" + cardPekka);
+        System.out.println("Brian" + cardBrian);
+        cardPekka.payEconomical();
+        cardPekka.payEconomical();
+        cardBrian.loadMoney(50);
+        System.out.println("Pekka" + cardPekka);
+        System.out.println("Brian" + cardBrian);
     }
 }
