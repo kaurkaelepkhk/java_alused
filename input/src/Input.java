ca0fb3bc6786f706d0abc9bba9b8d4f32af45957
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-22 08:46:13 +0300

    Exercise 73: Product

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,13 +2,7 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
-        Account accountA = new Account("A",100);
-        Account accountB = new Account("B",0);
-        Account accountC = new Account("C",0);
-        accountA.transfer(accountA, accountB, 50);
-        accountB.transfer(accountB, accountC, 25);
-        System.out.println(accountA);
-        System.out.println(accountB);
-        System.out.println(accountC);
+        Product banana = new Product("Banana", 1.1, 13);
+        banana.printProduct();
     }
 }
