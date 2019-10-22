9ac257a4bcfdd23a38f038c4c19697ebda18b98e
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-22 09:33:06 +0300

    Exercise 77.3: Balance nonnegative

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,7 +2,13 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
-        LyyraCard card = new LyyraCard(50);
+        LyyraCard card = new LyyraCard(5);
+        System.out.println(card);
+
+        card.payGourmet();
+        System.out.println(card);
+
+        card.payGourmet();
         System.out.println(card);
     }
 }
--- input/src/LyyraCard.java
+++ input/src/LyyraCard.java
@@ -4,6 +4,12 @@ public class LyyraCard {
     public LyyraCard(double balanceAtStart) {
         this.balance = balanceAtStart;
     }
+    public double payEconomical() {
+        return this.balance >= 2.50 ? this.balance -= 2.50 : this.balance;
+    }
+    public double payGourmet() {
+        return this.balance >= 4.00 ? this.balance -= 4.00 : this.balance;
+    }
     public String toString() {
         return "The card has " + this.balance + " euros";
     }
