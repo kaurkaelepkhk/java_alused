7de119aa27b7fe4ea8f89b2c4c3bae3a8d0fc1e0
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-28 11:59:56 +0200

    Exercise 78: Clock using a counter

--- input/src/BoundedCounter.java
+++ input/src/BoundedCounter.java
@@ -2,17 +2,34 @@ public class BoundedCounter {
     private int value;
     private int upperLimit;
 
-    public BoundedCounter(int upperLimit) {
+    public BoundedCounter(int upperLimit){
         this.upperLimit = upperLimit;
+        this.value = 0;
     }
-    public void next() {
-        if (this.value >= this.upperLimit) {
-            this.value = 0;
-        } else {
+
+    public void next(){
+        if(this.value<this.upperLimit){
             this.value++;
+        } else {
+            this.value = 0;
         }
     }
+
     public String toString() {
-        return this.value < 10 ? "0" + this.value : "" + this.value;
+        if(this.value<=9){
+            return "0"+value;
+        } else {
+            return ""+value;
+        }
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int newValue){
+        if (newValue>=0 && newValue<=this.upperLimit){
+            this.value = newValue;
+        }
     }
 }
