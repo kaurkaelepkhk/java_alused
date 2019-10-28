896621603841e18606b23ee8a2ea666d0a4d6649
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-10-28 12:33:48 +0200

    Exercise 79.4: Many sums

--- input/src/Input.java
+++ input/src/Input.java
@@ -2,14 +2,25 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
-        NumberStatistics stats = new NumberStatistics();
+        NumberStatistics sum = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
         Scanner reader = new Scanner(System.in);
         System.out.println("Type numbers");
         int input = 0;
         while (input != -1) {
-            stats.addNumber(input);
             input = reader.nextInt();
+            if(input != -1) {
+                sum.addNumber(input);
+                if(input % 2 == 0) {
+                    even.addNumber(input);
+                } else {
+                    odd.addNumber(input);
+                }
+            }
         }
-        System.out.println("sum: " + stats.sum());
+        System.out.println("sum: " + sum.sum());
+        System.out.println("sum of even: " + even.sum());
+        System.out.println("sum of odd: " + odd.sum());
     }
 }
